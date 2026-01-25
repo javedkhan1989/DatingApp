14b399c182b1a15199ccf3e8a4e2260c1a2ea37c
javedkhan1989 <jksoft2010@gmail.com>
2026-01-26 00:36:49 +0530

    section 13 26th Jan

--- API/Helpers/MemberParams.cs
+++ API/Helpers/MemberParams.cs
@@ -11,4 +11,5 @@ public class MemberParams : PagingParams
     public int MinAge { get; set; }=18;
 
     public int MaxAge { get; set; }=100;
+    public string OrderBy { get; set; } = "lastActive";
 }
